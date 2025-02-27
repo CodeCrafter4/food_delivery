c14d6e8ec644079572a0c193bb51004e29e9b3ed
muhammedAbdellayassin <ssswar367@gmail.com>
2025-02-27 14:26:15 +0300

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () =>{
     await mongoose
       .connect(
-        "mongodb+srv://ssswar367:u1vv4dasn3tJhP5n@cluster0.zmiew.mongodb.net/food-delivery"
+        process.env.DB
       )
       .then(() => {
         console.log("DB Connected");
